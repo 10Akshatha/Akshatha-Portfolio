65d733b23b286f939999eff338134c5eba17cbf0
10Akshatha <157686191+10Akshatha@users.noreply.github.com>
2026-08-12 12:22:11 +0530

    Add portfolio

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("mousemove", function (event) {
+    document.documentElement.style.setProperty(
+        "--mouse-x",
+        event.clientX + "px"
+    );
+
+    document.documentElement.style.setProperty(
+        "--mouse-y",
+        event.clientY + "px"
+    );
+});
